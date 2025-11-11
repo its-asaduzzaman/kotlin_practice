5d8760f45bd26489b958be52f5ce2f5e373e9bbf
its-asaduzzaman <samrat.asad@gmail.com>
2025-11-12 02:10:16 +0600

    Default parameters

--- main.kt
+++ main.kt
@@ -1,27 +1,37 @@
-fun main() {
-    val firstNumber = 10
-    val secondNumber = 5
-    val thirdNumber = 8
+/*fun main() {
+    val operatingSystem = "Chrome OS"
+    val emailId = "sample@gmail.com"
 
-    fun add(a: Int, b: Int): Int {
-        return a + b
+    fun displayAlertMessage(os: String, email: String): String {
+        return "There's a new sign-in request on $operatingSystem for your Google Account $emailId."
     }
 
-    fun sub(a: Int, b: Int): Int {
-        return a - b
-    }
-    
-    val result = add(firstNumber, secondNumber)
-    val anotherResult = add(firstNumber, thirdNumber)
-
-
-    println("$firstNumber + $secondNumber = $result")
-    println("$firstNumber + $thirdNumber = $anotherResult")
-
-    val result2 = sub(firstNumber, secondNumber)
-    val anotherResult2 = sub(firstNumber, thirdNumber)
-     println("$firstNumber - $secondNumber = $result2")
-    println("$firstNumber - $thirdNumber = $anotherResult2")
+    println(displayAlertMessage(operatingSystem, emailId))
 }
 
-// Define add() function below this line
+// Define your displayAlertMessage() below this line. */
+
+fun main() {
+    fun displayAlertMessage(operatingSystem: String = "Unknown OS", emailId: String): String {
+        return "There's a new sign-in request on $operatingSystem for your Google Account $emailId."
+    }
+
+    val firstUserEmailId = "user_one@gmail.com"
+
+    // The following line of code assumes that you named your parameter as emailId. 
+    // If you named it differently, feel free to update the name.
+    println(displayAlertMessage(emailId = firstUserEmailId))
+    println()
+
+    val secondUserOperatingSystem = "Windows"
+    val secondUserEmailId = "user_two@gmail.com"
+
+    println(displayAlertMessage(secondUserOperatingSystem, secondUserEmailId))
+    println()
+
+    val thirdUserOperatingSystem = "Mac OS"
+    val thirdUserEmailId = "user_three@gmail.com"
+
+    println(displayAlertMessage(thirdUserOperatingSystem, thirdUserEmailId))
+    println()
+}
