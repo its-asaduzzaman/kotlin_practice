e2b21f314f40ef7ef719df49de7c54a377761c7c
its-asaduzzaman <samrat.asad@gmail.com>
2025-11-12 02:19:11 +0600

    Pedometer

--- main.kt
+++ main.kt
@@ -1,37 +1,11 @@
-/*fun main() {
-    val operatingSystem = "Chrome OS"
-    val emailId = "sample@gmail.com"
-
-    fun displayAlertMessage(os: String, email: String): String {
-        return "There's a new sign-in request on $operatingSystem for your Google Account $emailId."
-    }
-
-    println(displayAlertMessage(operatingSystem, emailId))
+fun main() {
+    val steps = 4000
+    val caloriesBurned = PedometerStepsToCalories(steps);
+    println("Walking $steps steps burns $caloriesBurned calories") 
 }
 
-// Define your displayAlertMessage() below this line. */
-
-fun main() {
-    fun displayAlertMessage(operatingSystem: String = "Unknown OS", emailId: String): String {
-        return "There's a new sign-in request on $operatingSystem for your Google Account $emailId."
-    }
-
-    val firstUserEmailId = "user_one@gmail.com"
-
-    // The following line of code assumes that you named your parameter as emailId. 
-    // If you named it differently, feel free to update the name.
-    println(displayAlertMessage(emailId = firstUserEmailId))
-    println()
-
-    val secondUserOperatingSystem = "Windows"
-    val secondUserEmailId = "user_two@gmail.com"
-
-    println(displayAlertMessage(secondUserOperatingSystem, secondUserEmailId))
-    println()
-
-    val thirdUserOperatingSystem = "Mac OS"
-    val thirdUserEmailId = "user_three@gmail.com"
-
-    println(displayAlertMessage(thirdUserOperatingSystem, thirdUserEmailId))
-    println()
+fun PedometerStepsToCalories(numberOfSteps: Int): Double {
+    val caloriesBurnedForEachStep = 0.04
+    val totalCaloriesBurned = numberOfSteps * caloriesBurnedForEachStep
+    return totalCaloriesBurned
 }
