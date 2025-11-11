488ec6b872b32996bf05365dd3e332f3ead85165
its-asaduzzaman <samrat.asad@gmail.com>
2025-11-12 01:19:54 +0600

    Fix compile error

--- main.kt
+++ main.kt
@@ -1,3 +1,7 @@
-fun main() {
-    println( "Use the val keyword when the value doesn't change. \nUse the var keyword when the value can change. \nWhen you define a function, you define the parameters that can be passed to it. \nWhen you call a function, you pass arguments for the parameters.");
+/*fun main() { 
+    println("New chat message from a friend'}
+}*/
+
+fun main() { 
+    println("New chat message from a friend")
 }
