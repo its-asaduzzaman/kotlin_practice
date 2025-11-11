89df5e3220ea7c887bb652c09614615fb3e6f04e
its-asaduzzaman <samrat.asad@gmail.com>
2025-11-12 01:06:09 +0600

    Print messages

--- main.kt
+++ main.kt
@@ -1,3 +1,3 @@
 fun main() {
-    println("Hello, Kotlin from VS Code!")
+    println("Use the val keyword when the value doesn't change. \nUse the var keyword when the value can change. \nWhen you define a function, you define the parameters that can be passed to it. \nWhen you call a function, you pass arguments for the parameters. ");
 }
